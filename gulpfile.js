179cef070f7df3fa048a7cc64bb7fa3776680020
bhupendrasingh <bhupendrasingh@ccs.neu.edu>
2017-05-06 13:57:07 -0400

    more commands

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,18 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
 gulp.task('default',function(){
 	console.log("Hello World")
 });
+
+gulp.task('styles',function(){
+	gulp.src('sass/**/*.scss')
+	.pipe(sass().on('error',sass.logError))
+	.pipe(gulp.dest('./css'))
+});
+
+browserSync.init({
+	server:'/'
+});
+browserSync.create();
