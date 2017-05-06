ba048e83ebde20ecf75a28a4a6572970d09d9ba3
bhupendrasingh <bhupendrasingh@ccs.neu.edu>
2017-05-06 14:01:55 -0400

    modifications and watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
 
 gulp.task('default',function(){
 	console.log("Hello World")
@@ -12,7 +12,12 @@ gulp.task('styles',function(){
 	.pipe(gulp.dest('./css'))
 });
 
+gulp.task('change',function(){
+	gulp.watch('/sass/**/*.scss',['styles'])
+});
+
+
 browserSync.init({
 	server:'/'
 });
-browserSync.create();
+browserSync.stream();
